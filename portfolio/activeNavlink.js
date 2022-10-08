801f41f9121a1515287626a5d6410f4afdc01c4c
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-10-08 18:55:40 +0530

    portfolio-2 navlink is only based on scroll

--- portfolio/activeNavlink.js
+++ portfolio/activeNavlink.js
@@ -1,18 +1,34 @@
+const rem = Number.parseInt(
+    getComputedStyle(document.documentElement).fontSize.replace('px', '')
+);
+const navBarHeight = 4 * rem;
+
+// setting the active link whenever the section it links to reaches the top
 let prev = null;
+let lastScrollTop = 0;
+let hasGoneUp = {};
 
-// setting the active link on page load
-for (let a of document.getElementsByClassName('navlink')) {
-    if (a.getAttribute('href') === window.location.hash) {
-        a.classList.add('active-navlink');
-        prev = a;
+window.addEventListener('scroll', e => {
+    const currentScrollTop = document.documentElement.scrollTop;
+    const scrollingDown = currentScrollTop > lastScrollTop;
+
+    for (let section of document.getElementsByTagName('section')) {
+        const y = section.getBoundingClientRect().y;
+        const id = section.getAttribute('id');
+        if (scrollingDown && y <= navBarHeight && !hasGoneUp[id]) {
+            hasGoneUp[id] = true;
+            const link = document.querySelector(`a[href="#${id}"]`);
+            if (prev) prev.classList.remove('active-navlink');
+            link.classList.add('active-navlink');
+            prev = link;
+        } else if (!scrollingDown && y >= navBarHeight && hasGoneUp[id]) {
+            hasGoneUp[id] = false;
+            const link = document.querySelector(`a[href="#${id}"]`);
+            if (prev) prev.classList.remove('active-navlink');
+            link.classList.add('active-navlink');
+            prev = link;
+        }
     }
-}
 
-// setting the active link on every navlink click
-for (let a of document.getElementsByClassName('navlink')) {
-    a.addEventListener('click', () => {
-        if (prev) prev.classList.remove('active-navlink');
-        a.classList.add('active-navlink');
-        prev = a;
-    });
-}
+    lastScrollTop = currentScrollTop;
+});
