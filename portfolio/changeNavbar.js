2fd0a030e7a4701debb9af96ae6f103737eabe73
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-10-08 18:16:36 +0530

    fixed porfolio-2 navbar

--- portfolio/changeNavbar.js
+++ portfolio/changeNavbar.js
@@ -1,8 +1,14 @@
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navbar');
     const y = navbar.getBoundingClientRect().y;
-    if (y === 0 && !navbar.classList.contains('at-the-top'))
+    if (y === 0) {
         navbar.classList.add('at-the-top');
-    else if (y && navbar.classList.contains('at-the-top'))
+        navbar.classList.remove('near-the-top');
+    } else if (y < 50) {
+        navbar.classList.add('near-the-top');
         navbar.classList.remove('at-the-top');
+    } else {
+        navbar.classList.remove('at-the-top');
+        navbar.classList.remove('near-the-top');
+    }
 });
